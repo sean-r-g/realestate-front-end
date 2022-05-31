3c0156cfd62e92e2753cfedacc5e8d6a9bd48298
ThatUche <newuser@Uches-MacBook-Air.local>
2022-05-31 16:17:25 -0400

    house front end first commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,25 +4,20 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 import Houses from './components/houses'
 import Condos from './components/condos'
-
 function App() {
-
-
-
-
-
-
-
-
-
-
+  const [displayHouses, setDisplayHouses] = useState(true)
+  const toggleDisplay = () =>{
+    displayHouses ? setDisplayHouses(false) : setDisplayHouses(true)
+  }
   return (
     <>
-      <h1>Okeke-Gillis Realty</h1>
-      <Houses house={house}/>
-      <Condos condo={condo}/>
+      <header>
+      { displayHouses ? <button className='display-btn' onClick={toggleDisplay}>Show Condos</button> : <button className='display-btn' onClick={toggleDisplay}>Show Houses</button>}
+      {/* <h1>Okeke-Gillis Realty</h1> */}
+      <img src='https://i.imgur.com/P163pZY.png'/>
+      </header>
+      { displayHouses ? <Houses/> : <Condos/>}
     </>
   );
 }
-
 export default App;
--- front-end/src/components/houses.js
+++ front-end/src/components/houses.js
@@ -5,12 +5,132 @@ import axios from 'axios'
 
 const Houses = (props) => {
 
+  const [newName, setNewName] = useState()
+  const [newAddress, setNewAddress] = useState ()
+  const [price, setPrice] = useState ()
+  const [size, setSize] = useState ()
+  const [roomsNum, setRoomsNum] = useState ()
+  const [bathNum, setBathNum] = useState ()
+  const [newImage, setNewImage] = useState ()
+  const [newAvailable, setNewAvailable] = useState (true)
+  const [showEdit, setShowEdit] = useState (false)
   const [houses, setHouses] = useState([])
 
-  return (
-    <div>
+  const handleNewImage = (event) => {
+    setNewImage(event.target.value)
+  }
 
-    </div>
+  const handleNewAddress = (event) => {
+    setNewAddress(event.target.value)
+  }
+
+  const handleNewPrice = (event) => {
+    setPrice(event.target.value)
+  }
+
+  const handleNewSize = (event) => {
+    setSize(event.target.value)
+  }
+
+  const handleNewRoomsNum = (event) => {
+    setRoomsNum(event.target.value)
+  }
+
+  const handleNewBathsNum = (event) => {
+    setBathNum(event.target.value)
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+
+  const handleNewHouseSubmit = (event) => {
+    event.preventDefault()
+    axios.post(
+      'http://localhost:3000/houses', {
+        name: newName,
+        location: newAddress,
+        price: price,
+        size: size,
+        rooms: roomsNum,
+        bath: bathNum,
+        image: newImage,
+        available: newAvailable,
+        showEdit: showEdit
+      }
+    ).then(() => {
+      axios.get('http://localhost:3000/houses').then((response) => {
+        setHouses(response.data)
+      })
+    })
+  }
+
+  const handleHousesUpdate = (event, houseData) => {
+    event.preventDefault()
+      axios
+      .put(`http://localhost:3000/houses/${houseData._id}`,
+      {
+        name: newName,
+        location: newAddress,
+        price: price,
+        size: size,
+        rooms: roomsNum,
+        bath: bathNum,
+        image: newImage
+      }).then(() => {
+          axios
+            .get('http://localhost:3000/houses')
+            .then((response) => {
+              setHouses(response.data)
+            })
+      })
+  }
+
+  
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/albums').then((response) => {
+      setHouses(response.data)
+    })
+  }, [])
+
+  return (
+    <main>
+      <h1>List a House</h1>
+      <section>
+        <form onSubmit={handleNewHouseSubmit}>
+          <input placeholder='Image Link' onChange={handleNewImage} type='text'/><br/>         
+          <input placeholder='Address' onChange={handleNewAddress} type='text'/><br/>         
+          <input placeholder='Price' onChange={handleNewPrice} type='number'/><br/>         
+          <input placeholder='Size' onChange={handleNewSize} type='number'/><br/>         
+          <input placeholder='Rooms number' onChange={handleNewRoomsNum} type='number'/><br/>         
+          <input placeholder='Baths number' onChange={handleNewBathsNum} type='number'/><br/>         
+          <input placeholder='House name' onChange={handleNewName} type='text'/><br/>         
+          <input type="submit" value="Create New Listing"/><br/>     
+        </form>
+      </section>
+      <section className='container'>
+        {houses.map((house) => {
+          return (
+            <div className='card' key={houses._id}>
+              <img src={house.image}/>
+              <h2>{house.name}</h2>
+              <h3>Location: {house.location}</h3>
+              <h3>Price: {house.price}</h3>
+              <h3>Size: {house.size}</h3>
+              <h3>{house.rooms} Rooms, {house.bath} bathrooms</h3>
+              {house.available ? <h4>Status: Available</h4> : <h4>Status: Unavailable</h4>}
+              <button id='edit-btn'>Edit Listing</button>
+                <form>
+
+                </form>
+            </div>
+          )
+        })}
+      </section>
+    </main>
   )
 
 }
