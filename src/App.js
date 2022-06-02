c95967e7164e5fbd4723217f55a325b638d7227a
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-06-02 14:11:35 -0500

    fixed display toggle button to default to show houses

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
       @import url('https://fonts.googleapis.com/css2?family=Paytone+One&display=swap');
       </style>
       <header>
-      { displayCondos ? <button className='display-btn' onClick={showHouses}>Show Houses</button> : <button className='display-btn' onClick={showCondos}>Show Condos</button>}
+      { displayHouses ? <button className='display-btn' onClick={showCondos}>Show Condos</button> : <button className='display-btn' onClick={showHouses}>Show Houses</button>}
       <button id='about-btn' onClick={showIntro}>About</button>
       <img id='logo' src='https://i.imgur.com/J8OBU5K.png'/>
       </header>
