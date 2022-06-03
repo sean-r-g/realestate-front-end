f29c2ecf35c08f3e02edefa202ce09250faadec7
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-06-03 15:27:04 -0500

    adding carousel

--- src/App.js
+++ src/App.js
@@ -33,15 +33,41 @@ function App() {
   const Introduction = () =>{
     return (
       <div className='primary-div' id='about-div'>
-        <h1>About our platform...</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur est augue, placerat id nisl at, pretium accumsan ligula. Praesent dictum justo ante, et iaculis lorem euismod id. Pellentesque dictum neque malesuada viverra laoreet. Aliquam libero turpis, consectetur a metus vitae, suscipit porta lorem. Vivamus mollis, odio at fringilla finibus, ante justo tincidunt sapien, in pellentesque purus nisi ut turpis. Aenean porttitor sem felis, quis finibus ligula vehicula in. Nulla sed ultrices nibh. Nullam a leo dui.</p>
-        <p>Maecenas vitae ex diam. Nulla facilisi. Ut sed ante mi. Aenean quis libero gravida, semper elit ac, tincidunt ligula. Mauris sit amet eros hendrerit, euismod arcu ut, fermentum ipsum. Curabitur vitae congue magna, sed hendrerit sapien. Aliquam lobortis gravida congue. Phasellus maximus tempor odio et vestibulum. Nullam quam sapien, euismod eleifend lacinia nec, sodales vel enim. Etiam ut iaculis ante, eget vehicula est. In viverra eget sem non luctus. Pellentesque leo ligula, dictum in accumsan vitae, bibendum non ex.
+        <h1>Browse. List. Update.</h1>
+        <p>Welcome to realitive - an open community that allows prospective home owners and sellers to browse an ever-changing collection of houses and condos.</p>
+        <p>Feel free to review the current listings, add additional ones, or update existing homes as appropriate.
         </p>
         <div className='button-div'>
           <button onClick={showHouses}>Houses</button>
           <button onClick={showCondos}>Condos</button>
         </div>
+        <div class='slides-container'>
+    <div class='slides-container'>
+      <div class='previous'>
+        <span>
+          <a href="#" class="previousbtn">&laquo;</a>
+        </span>
       </div>
+      <div class='project-screenshots'>
+        <img class='screenshots' src='https://www.housedigest.com/img/gallery/exterior-house-colors-you-should-avoid-using-at-all-costs/intro-1619022773.webp'>
+        </img>
+        <img class='screenshots' src='https://media.istockphoto.com/photos/single-family-new-construction-home-in-suburb-neighborhood-in-the-picture-id1147674296?k=20&m=1147674296&s=612x612&w=0&h=Nk11QdlAqLZQEkOx1DDKcWJoSnQn29AjdUQpKeK_GCs='>
+        </img>
+        <img class='screenshots' src='https://images.ctfassets.net/pg6xj64qk0kh/2r4QaBLvhQFH1mPGljSdR9/39b737d93854060282f6b4a9b9893202/camden-paces-apartments-buckhead-ga-terraces-living-room-with-den_1.jpg'>
+        </img>
+        <img class='screenshots' src='https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/stock%2FGettyImages-1164596018_copy'>
+        </img>
+        <img class='screenshots'src='https://image1.apartmentfinder.com/i2/vGcr85sYkBnOMY6spmFvM_yKIn6gy0XYy6Nl8SvtQnM/110/image.jpg'/>
+      </div>
+      <div class='next'>
+        <span>
+          <a href="#" class="nextbtn">&raquo;</a>
+        </span>
+      </div>
+    </div>
+  </div>
+      </div>
+      
     )
   }
 
